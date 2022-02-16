01776353303dcd99c3fff18e474dcf6613f83845
herrickc9614 <46943963+herrickc9614@users.noreply.github.com>
2022-02-16 14:54:30 -0600

    Created structure for webpage

--- src/App.js
+++ src/App.js
@@ -1,49 +1,13 @@
-import React from 'react';
-import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';  
+import './App.css';  
+import Layout  from './Layout/Layout'   
 
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
-import { LinkContainer } from 'react-router-bootstrap';
-
-import './App.css';
-
-const Home = () => <span>Home</span>;
-
-const About = () => <span>About</span>;
-
-const Users = () => <span>Users</span>;
-
-const App = () => (
-  <MemoryRouter>
-    <Container className="p-3">
-      <Container className="p-5 mb-4 bg-light rounded-3">
-        <h1 className="header">Welcome To React-Bootstrap</h1>
-        <h2>
-          Current Page is{' '}
-          <Routes>
-            <Route path="/about" element={<About />}/>
-            <Route path="/users" element={<Users />}/>
-            <Route path="/" element={<Home />}/>
-          </Routes>
-        </h2>
-        <h2>
-          Navigate to{' '}
-          <ButtonToolbar className="custom-btn-toolbar">
-            <LinkContainer to="/">
-              <Button>Home</Button>
-            </LinkContainer>
-            <LinkContainer to="/about">
-              <Button>About</Button>
-            </LinkContainer>
-            <LinkContainer to="/users">
-              <Button>Users</Button>
-            </LinkContainer>
-          </ButtonToolbar>
-        </h2>
-      </Container>
-    </Container>
-  </MemoryRouter>
-);
-
-export default App;
+function App() {  
+  return (  
+     <div className="App">  
+      <Layout/>   
+     </div>  
+  );  
+}  
+  
+export default App;  
---
+++ src/Layout/Layout.js
@@ -0,0 +1,25 @@
+import React, { Component} from 'react';  
+import Leftside from './Leftside';  
+import Header from './Header'  
+import Footer from './Footer'  
+
+export class Layout extends Component {  
+    loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>  
+    render() {  
+        return (  
+            <div>  
+                <div id="wrapper">  
+                    <Leftside></Leftside>  
+                    <div id="content-wrapper" class="d-flex flex-column">  
+                        <div id="content">  
+                            <Header />  
+                        </div>  
+                        <Footer />  
+                    </div>  
+                </div>  
+            </div>  
+        )  
+    }  
+}  
+  
+export default Layout  
